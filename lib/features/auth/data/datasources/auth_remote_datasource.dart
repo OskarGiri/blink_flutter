f2dec0afc321b8900e512c6c31c1b914a7bb7dc7
Oscar giri <oskargiri87@gamil.com>
2026-01-16 13:31:21 +0545

    auth remote

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -4,25 +4,23 @@ class AuthRemoteDataSource {
   final Dio dio;
   AuthRemoteDataSource(this.dio);
 
-  Future<Map<String, dynamic>> signup({
+  Future<void> signup({
     required String email,
     required String password,
   }) async {
-    final res = await dio.post(
+    await dio.post(
       '/auth/signup',
       data: {'email': email, 'password': password},
     );
-    return Map<String, dynamic>.from(res.data);
   }
 
-  Future<Map<String, dynamic>> login({
+  Future<void> login({
     required String email,
     required String password,
   }) async {
-    final res = await dio.post(
+    await dio.post(
       '/auth/login',
       data: {'email': email, 'password': password},
     );
-    return Map<String, dynamic>.from(res.data);
   }
 }
